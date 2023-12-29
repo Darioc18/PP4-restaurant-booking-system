aa4b72e2a34a57c61a5606aeb1488d8fc23be160
Dario <dariocovella@gmail.com>
2023-12-29 02:58:50 +0000

    update allowed hosts in settings.py for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pp4-restaurant-booking-system-d75be81c2fba.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['pp4-restaurant-booking-system-d75be81c2fba.herokuapp.com', 'localhost',]
 
 
 # Application definition
