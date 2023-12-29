da54713b76885c514cf61aac7c21a7a384d6115b
Dario <dariocovella@gmail.com>
2023-12-29 16:53:21 +0000

    build admin site

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Reservation
+
 
 # Register your models here.
+@admin.register(Reservation)
+class BookingAdmin(admin.ModelAdmin):
+    """
+    class to filter and display info of the reservations
+    """
+    list_filter = ('update_on',)
+    list_display = ('id', 'full_name', 'num_of_guests', 'notes')
