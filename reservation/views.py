b14e04e4a048b54f55319a63ae3564e0c12bccb9
Dario <dariocovella@gmail.com>
2024-01-02 01:15:13 +0000

    create preliminary view code

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Reservation
 
 # Create your views here.
+
+class Home(generic.DetailView):
+    """
+    Renders the Index page in the browser
+    """
+    def get(self, request):
+        return render(request, 'index.html')
+    
+    
+class Menu(generic.DetailView):
+    """
+    Renders the Menu page in the browser
+    """
+    def get(self, request):
+        return render(request, 'menu.html')
