94f18f6c08bb8f7f9f86d33411c59257523d1a4f
Dario <dariocovella@gmail.com>
2024-01-03 02:03:27 +0000

    create view for the reservation form

--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Reservation
+from django.views.generic.edit import CreateView
+from .forms import ReservationForm  # Create a form for the Reservation model
 
 # Create your views here.
 
@@ -18,3 +20,9 @@ class Menu(generic.DetailView):
     """
     def get(self, request):
         return render(request, 'menu.html')
+    
+class BookingView(CreateView):
+    model = Reservation
+    form_class = ReservationForm
+    template_name = 'booking.html'
+    success_url = '/booking_success/'
