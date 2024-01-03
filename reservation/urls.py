db44d6b4d3f9a525e905f9508b955440c6b23158
Dario <dariocovella@gmail.com>
2024-01-03 02:06:31 +0000

    add the booking url pattern to urls.py

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('menu/', views.Menu.as_view(), name='menu'),
+    path('booking/', views.BookingView.as_view(), name='booking'),  # Add the booking URL pattern
 ]
