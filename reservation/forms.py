4bb9b489789a75aabded9abb9b2ad540c2e5754e
Dario <dariocovella@gmail.com>
2024-01-06 16:07:17 +0000

    add calendar widget to booking form

--- reservation/forms.py
+++ reservation/forms.py
@@ -2,8 +2,12 @@ from django import forms
 from .models import Reservation
 
 class ReservationForm(forms.ModelForm):
+    """Form to book a table"""
     class Meta:
+        """Meta class"""
         model = Reservation
         fields = '__all__'
         exclude = ['author']
-        
+        widgets = {
+            'date_of_booking': forms.DateInput(attrs={'type': 'datetime-local'}),
+        }
