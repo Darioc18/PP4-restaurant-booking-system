ee47ef182629e00d7d79cc5556957e0622915462
Dario <dariocovella@gmail.com>
2024-01-06 16:06:30 +0000

    allow saving bookings to database

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 from .models import Reservation
@@ -22,7 +23,21 @@ class Menu(generic.DetailView):
         return render(request, 'menu.html')
     
 class BookingView(CreateView):
-    model = Reservation
+    # model = Reservation
     form_class = ReservationForm
     template_name = 'booking.html'
-    success_url = '/booking_success/'
+    success_message = "Booking successful!"
+
+    def booking_view(self, request):
+        return render(request, 'booking.html')
+
+    def post(self, request):
+        form = ReservationForm(data=request.POST)
+
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            reservation.author = request.user
+            reservation.save()
+            return HttpResponse("Thank you for your booking. Your reservation is successful.")
+        else:
+            return HttpResponse("Booking incomplete. Please verify your booking details.")
