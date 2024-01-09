ec86fe3e9f36996a370cd02f39cabaadea6493ea
Dario <dariocovella@gmail.com>
2024-01-09 17:27:20 +0000

    update debug variable

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = ['pp4-restaurant-booking-system-d75be81c2fba.herokuapp.com', 'localhost', '127.0.0.1']
 
