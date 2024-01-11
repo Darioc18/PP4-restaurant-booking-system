1ae3b38cec95cce5812f95b3a9482fe2fc44ffeb
Dario <dariocovella@gmail.com>
2024-01-11 06:31:00 +0000

    Update error message in views.py

--- reservation/views.py
+++ reservation/views.py
@@ -41,7 +41,7 @@ class BookingView(CreateView):
 
             return redirect('confirmation_page') # Redirect to the confirmation page
         else:
-            messages.error(request, "Booking incomplete. Please check you booking date is not in the past and your number of guests is between 1 and 10.")
+            messages.error(request, "Invalid booking. Please ensure your booking date is not in the past.")
         return redirect('booking')  # Redirect to the booking page with errors
         
 class UserReservationsList(View):
